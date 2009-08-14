56e107c187653ded83d15f8c3e336180aa171b11
AntoineVanGelderJnr <AntoineVanGelderJnr@5035072a-db25-11dd-9f21-5bfbb98aebe5>
2009-08-14 17:36:00 +0000

    *bump*

--- trunk/package-scripts/debian/pbuilder-update-afrimesh-ppa-keys.sh
+++ trunk/package-scripts/debian/pbuilder-update-afrimesh-ppa-keys.sh
@@ -2,24 +2,21 @@
 
 # update pbuilder with public keys to be able to use launchpad PPA's to resolve depends
 
-# pubkey for afrimesh PPA
 echo
 echo "Fetching public key for Afrimesh PPA"
 echo
-gpg --keyserver keyserver.ubuntu.com --recv 382AF1D2
-gpg --export --armor 382AF1D2 | sudo apt-key add -
+gpg --keyserver keyserver.ubuntu.com --recv 50CFBA3E
+gpg --export --armor 50CFBA3E | sudo apt-key add -
 
-#echo "deb http://ppa.launchpad.net/afrimesh/ppa/ubuntu hardy main" >> /etc/apt/sources.list
-#echo "deb-src http://ppa.launchpad.net/afrimesh/ppa/ubuntu hardy main" >> /etc/apt/sources.list
-echo "deb http://ppa.launchpad.net/antoine-7degrees/ppa/ubuntu hardy main" >> /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/antoine-7degrees/ppa/ubuntu hardy main" >> /etc/apt/sources.list
+echo "deb http://ppa.launchpad.net/afrimesh/ppa/ubuntu hardy main" >> /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/afrimesh/ppa/ubuntu hardy main" >> /etc/apt/sources.list
 
 echo
 echo "Updating repositories"
 echo
 
 sudo apt-get update
-# sudo apt-get --yes install uci-dev json-c-dev
+
 echo
 echo "Finished adding Afrimesh PPA"
 echo 
