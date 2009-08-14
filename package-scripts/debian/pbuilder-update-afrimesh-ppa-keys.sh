548d54553bd99cde629f6299dbae827db470dc49
AntoineVanGelderJnr <AntoineVanGelderJnr@5035072a-db25-11dd-9f21-5bfbb98aebe5>
2009-08-14 15:04:44 +0000

    More work on supporting multiple PPA's Deprecated config/mesh

--- package-scripts/debian/pbuilder-update-afrimesh-ppa-keys.sh
+++ package-scripts/debian/pbuilder-update-afrimesh-ppa-keys.sh
@@ -9,8 +9,10 @@ echo
 gpg --keyserver keyserver.ubuntu.com --recv 382AF1D2
 gpg --export --armor 382AF1D2 | sudo apt-key add -
 
-echo "deb http://ppa.launchpad.net/afrimesh/ppa/ubuntu hardy main" >> /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/afrimesh/ppa/ubuntu hardy main" >> /etc/apt/sources.list
+#echo "deb http://ppa.launchpad.net/afrimesh/ppa/ubuntu hardy main" >> /etc/apt/sources.list
+#echo "deb-src http://ppa.launchpad.net/afrimesh/ppa/ubuntu hardy main" >> /etc/apt/sources.list
+echo "deb http://ppa.launchpad.net/antoine-7degrees/ppa/ubuntu hardy main" >> /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/antoine-7degrees/ppa/ubuntu hardy main" >> /etc/apt/sources.list
 
 echo
 echo "Updating repositories"
