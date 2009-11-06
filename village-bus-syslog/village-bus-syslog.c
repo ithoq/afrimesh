db6fbe8dcb0571dee0675e919cd6dd11eeac454a
AntoineVanGelderJnr <AntoineVanGelderJnr@5035072a-db25-11dd-9f21-5bfbb98aebe5>
2009-11-06 11:55:21 +0000

    Some cleanup for village-bus.js Initial checkin for village-bus-syslog

---
+++ village-bus-syslog/village-bus-syslog.c
@@ -0,0 +1,131 @@
+/**
+ * Afrimesh: easy management for B.A.T.M.A.N. wireless mesh networks
+ * Copyright (C) 2008-2009 Meraka Institute of the CSIR
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+
+#include "village-bus-syslog.h"
+
+
+struct json_object* die_gracefully(FILE* file, struct json_object* error)
+{
+  fclose(file);
+  return error;
+}
+
+
+struct json_object* readlog(int n)
+{
+  size_t readsize = 256;
+  char   buffer[readsize];
+  struct json_object* entries = json_object_new_array();
+
+  /* TODO - find logfile */
+  //const char* name = "/var/log/system.log";
+  const char* name = "/var/log/messages";
+
+  /* open logfile */
+  FILE* logfile = fopen(name, "r");
+  if (!logfile) {
+    return jsonrpc_error("Failed to open system log: %s", strerror(errno));
+  }
+
+  /* find eof */
+  if (fseek(logfile, 0, SEEK_END) == -1) {
+    return die_gracefully(logfile, jsonrpc_error("Seek failed - %s", strerror(errno)));
+  }
+  long eof = ftell(logfile);
+  if (eof == -1) {
+    return die_gracefully(logfile, jsonrpc_error("Could not locate end of logfile - %s", strerror(errno)));
+  }
+  
+  long eol = eof;
+  size_t lines = 0;
+  for (lines = 0; lines < n; lines++) {
+
+    /* go to last known line end & read previous 'readsize' bytes */
+    if (fseek(logfile, eol - readsize, SEEK_SET) == -1) {
+      return die_gracefully(logfile, jsonrpc_error("Seek failed - %s", strerror(errno)));
+    }
+    size_t bytesread = fread(buffer, 1, readsize, logfile);
+    if (bytesread == -1) {
+      return die_gracefully(logfile, jsonrpc_error("Read failed - %s", strerror(errno)));
+    }
+    buffer[bytesread] = 0;
+
+    /* read buffer backward until there's a newline */
+    char* start;
+    for (start = buffer + bytesread - 2; start != buffer; start--) {
+      if (*start == '\n') {
+        break;
+      }
+    }
+
+    /* everything between here and end of buffer is the line */
+    size_t line_length = (buffer + bytesread) - start;
+    start[line_length] = 0;
+    /* TODO - parse line */
+    //printf("-> %d : |%s|\n", lines, start + 1);
+    json_object_array_add(entries, json_object_new_string(start + 1));
+    
+    /* move last known eol backward */
+    eol = eol - line_length;
+  }
+
+  /* cleanup */
+  if (fclose(logfile) != 0) {
+    return jsonrpc_error("Error closing system log: %s", strerror(errno));
+  }
+
+  struct json_object* response = json_object_new_object();
+  json_object_object_add(response, "result", entries);
+  return response;
+}
+
+
+  /*
+  printf("%s\n\n", buffer);*/
+  /*char* line;
+  size_t length;
+  int lineno = 0;
+  while ((line = fgetln(logfile, &length)) != NULL) {
+    line[length-1] = 0;
+    printf("-> %d: %s\n", lineno++, line);
+  }*/
+
+
+  
+  /* read last 'readsize' bytes of file */
+  /*if (fseek(logfile, eof - readsize, SEEK_SET) == -1) {
+    return die_gracefully(logfile, jsonrpc_error("Seek failed - %s", strerror(errno)));
+    }*/
+
--- village-bus/main.c
+++ village-bus/main.c
@@ -36,10 +36,11 @@
  * JSON/RPC method registration and interface specification
  */
 struct MethodDispatch dispatch_table[] = {
-  { "/uci",  "show", {json_type_string}, 1, jsonrpc_dispatch_uci_show },
-  { "/uci",  "set",  {json_type_array }, 1, jsonrpc_dispatch_uci_set  },
-  { "/snmp", "get",  {json_type_string, json_type_string, json_type_array }, 3, jsonrpc_dispatch_snmp },
-  { "/snmp", "walk", {json_type_string, json_type_string, json_type_string}, 3, jsonrpc_dispatch_snmp },
+  { "/uci",    "show", {json_type_string}, 1, jsonrpc_dispatch_uci_show },
+  { "/uci",    "set",  {json_type_array }, 1, jsonrpc_dispatch_uci_set  },
+  { "/snmp",   "get",  {json_type_string, json_type_string, json_type_array }, 3, jsonrpc_dispatch_snmp   },
+  { "/snmp",   "walk", {json_type_string, json_type_string, json_type_string}, 3, jsonrpc_dispatch_snmp   },
+  { "/syslog", "read", {json_type_int},   1, jsonrpc_dispatch_syslog_read },
   { 0, 0, 0, 0 }
 };
 
