eba1967fae6a542c675de6ff06cc7cc70234c56f
AntoineVanGelderJnr <AntoineVanGelderJnr@5035072a-db25-11dd-9f21-5bfbb98aebe5>
2010-07-19 10:43:20 +0000

    Deprecating afrimesh.system

--- branches/villagetelco/dashboard/javascript/afrimesh.system.js
+++ branches/villagetelco/dashboard/javascript/afrimesh.system.js
@@ -13,19 +13,6 @@ function BootSystem(parent) {
   var system = function(address, continuation) { return this.system.uname(address, continuation); };
 
 
-  /**
-   * Returns the system log
-   */
-  system.log = function(continuation, count) {
-    var name = afrimesh.villagebus.Name("/@root/sys/syslog");
-    name = afrimesh.villagebus.Bind(name, function(error, response) {
-        if (error) return continuation(error, null); // TODO - return Fail(error, continuation) maybe ?
-        return continuation(error, response);
-      });
-    name = afrimesh.villagebus.GET(name, { count : (count?count:10) });
-    return name;
-  };
-
   
   return system;
 };
