e59a0249e7283e84998e35166c08155890ae7a2b
AntoineVanGelderJnr <AntoineVanGelderJnr@5035072a-db25-11dd-9f21-5bfbb98aebe5>
2010-07-24 12:58:22 +0000

    Bumped rest of Ubuntu packaging to lucid

--- branches/villagetelco/package-scripts/debian/pbuilder-update-afrimesh-ppa-keys.sh
+++ branches/villagetelco/package-scripts/debian/pbuilder-update-afrimesh-ppa-keys.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# update pbuilder with public keys to be able to use launchpad PPA's to resolve depends
-
 echo
-echo "Fetching public key for Afrimesh PPA"
+echo "Adding Afrimesh PPA"
 echo
-gpg --keyserver keyserver.ubuntu.com --recv 50CFBA3E
-gpg --export --armor 50CFBA3E | sudo apt-key add -
 
-echo "deb http://ppa.launchpad.net/afrimesh/ppa/ubuntu hardy main" >> /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/afrimesh/ppa/ubuntu hardy main" >> /etc/apt/sources.list
+sudo apt-add-repository http://ppa.launchpad.net/afrimesh/ppa/ubuntu lucid main
+
+## update pbuilder with public keys to be able to use launchpad PPA's to resolve depends
+#gpg --keyserver keyserver.ubuntu.com --recv 50CFBA3E
+#gpg --export --armor 50CFBA3E | sudo apt-key add -
+
+#echo "deb http://ppa.launchpad.net/afrimesh/ppa/ubuntu lucid main" >> /etc/apt/sources.list
+#echo "deb-src http://ppa.launchpad.net/afrimesh/ppa/ubuntu lucid main" >> /etc/apt/sources.list
 
 echo
 echo "Updating repositories"
--- branches/villagetelco/package-scripts/debian/pbuilder-update-antoine-7degrees-ppa-keys.sh
+++ branches/villagetelco/package-scripts/debian/pbuilder-update-antoine-7degrees-ppa-keys.sh
@@ -2,19 +2,22 @@
 
 # update pbuilder with public keys to be able to use launchpad PPA's to resolve depends
 
-# pubkey for afrimesh PPA
 echo
-echo "Fetching public key for Afrimesh PPA"
+echo "Adding Afrimesh PPA"
 echo
-#whoami
-#echo
-#mkdir -p /home/$USER/.gnupg
-#mkdir -p /home/antoine/.gnupg
-gpg --keyserver keyserver.ubuntu.com --recv 382AF1D2
-gpg --export --armor 382AF1D2 | sudo apt-key add -
 
-echo "deb http://ppa.launchpad.net/antoine-7degrees/ppa/ubuntu hardy main" >> /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/antoine-7degrees/ppa/ubuntu hardy main" >> /etc/apt/sources.list
+sudo apt-add-repository http://ppa.launchpad.net/antoine-7degrees/ppa/ubuntu lucid main
+
+# pubkey for afrimesh PPA
+##whoami
+##echo
+##mkdir -p /home/$USER/.gnupg
+##mkdir -p /home/antoine/.gnupg
+#gpg --keyserver keyserver.ubuntu.com --recv 382AF1D2
+#gpg --export --armor 382AF1D2 | sudo apt-key add -
+
+#echo "deb http://ppa.launchpad.net/antoine-7degrees/ppa/ubuntu lucid main" >> /etc/apt/sources.list
+#echo "deb-src http://ppa.launchpad.net/antoine-7degrees/ppa/ubuntu lucid main" >> /etc/apt/sources.list
 
 echo
 echo "Updating repositories"
