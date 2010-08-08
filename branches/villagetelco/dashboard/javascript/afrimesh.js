c161e4950588c7891be20670e00adb534f6707ee
AntoineVanGelderJnr <AntoineVanGelderJnr@5035072a-db25-11dd-9f21-5bfbb98aebe5>
2010-08-08 11:44:19 +0000

    Added lon/lat labels for dashboard location fields Refactored notification event handling Provisioning multiple routers no longer creates duplicate accounts Added 'home' icon to toolbox to hide overlay & show map

--- branches/villagetelco/dashboard/javascript/afrimesh.js
+++ branches/villagetelco/dashboard/javascript/afrimesh.js
@@ -15,8 +15,8 @@ var afrimesh = undefined;
   function Afrimesh() {
     console.log("Window Location: " + window.location.hostname);
     this.settings = { 
-      address      : window.location.hostname ? window.location.hostname : "196.211.3.106",
-      //address      : window.location.hostname ? window.location.hostname : "10.0.0.2",
+      //address      : window.location.hostname ? window.location.hostname : "196.211.3.106",
+      address      : window.location.hostname ? window.location.hostname : "192.168.20.108",
       ajax_proxy   : "/cgi-bin/ajax-proxy.cgi?url="
     };
     console.log("this.settings.address : " + this.settings.address);
