74bdd6944066b375a5d233ce1d2d5b70c26f6bd7
AntoineVanGelderJnr <AntoineVanGelderJnr@5035072a-db25-11dd-9f21-5bfbb98aebe5>
2010-08-18 15:05:10 +0000

    Load multiple API dependencies & block until completion.

--- branches/villagetelco/dashboard/javascript/afrimesh.api.js
+++ branches/villagetelco/dashboard/javascript/afrimesh.api.js
@@ -22,23 +22,57 @@ if (typeof XMLHttpRequest == "undefined") {
 
 /** - require ----------------------------------------------------------- */
 // Also see: http://unixpapa.com/js/dyna.html
-function _require(name) {
+function _require(names, async, continuation) {
+  if (typeof names === "string") {
+    return _getScript(names, async, continuation);
+  } else if (Object.prototype.toString.call(names) === "[object Array]") {
+    console.log("Loading multiple names: " + names);
+  } else {
+    console.log("Invalid name: " + names);
+    return;
+  }
+  var pool = [];
+  for (var n in names) {
+    pool.push(_getScript(names[n], async));
+  }
+  function poll(p) {
+    var done = p.every(function(request) { return request.done == true; })
+    if (done) return continuation();
+    setTimeout(function() { poll(p); }, 100);
+  };
+  poll(pool);
+
+};
+
+function _getScript(name, async, continuation) {
   var request = new XMLHttpRequest();    
   if (!request) {
     return console.log("Can't load: " + name);
   }
-  request.open("GET", name, false); // true=async, false=sync
+  request.onreadystatechange = function() {
+    /*console.log("readyState: " + request.readyState);
+    if (request.readyState > 1) {
+      console.log("status: " + request.status);
+    }*/
+    if (request.readyState == 4 && request.status == 200) {
+      var result = eval(request.responseText);
+      request.done = true;
+      if (continuation) {
+        continuation(request.responseText);
+      }
+    }
+  };
+  request.open("GET", name, async); 
   request.send();
-  var result = eval(request.responseText);
-  console.log(name + " -> " + result);
-};
+  return request;
+}
 
 
 /** - jquery ------------------------------------------------------------ */
-_require("javascript/jquery/jquery.min.js");
+_require("javascript/jquery/jquery.min.js", false);
 
 
-/** - afrimesh ---------------------------------------------------------- */
+/** - afrimesh.ready() -------------------------------------------------- */
 var afrimesh = {};
 afrimesh._ready = function() {
   console.log("default readyfn");
@@ -48,10 +82,25 @@ afrimesh.ready = function(f) {
 };
 
 
-
-
-/** - fin --------------------------------------------------------------- */
+/** - afrimesh bootstrap ------------------------------------------------ */
 $(document).ready(function() {
-    afrimesh._ready();
+    console.log("jquery ready");
+    _require(["javascript/afrimesh.utilities.js",
+              "javascript/afrimesh.platform.js",
+              "javascript/afrimesh.villagebus.js",
+              "javascript/afrimesh.storage.js", 
+              "javascript/afrimesh.network.js", 
+              "javascript/afrimesh.device.js",
+              "javascript/afrimesh.community.js",
+              "javascript/afrimesh.person.js", 
+              "javascript/afrimesh.customers.js", 
+              "javascript/afrimesh.telephony.js", 
+              "javascript/afrimesh.settings.js" ], 
+             true, 
+             function(data) {
+                 //_require("javascript/afrimesh.js", false);
+                 //afrimesh.storage = BootStorage(afrimesh);
+                 afrimesh._ready(); 
+               });
   });
 console.log("afrimesh.api.js");
