a260bd69a9ec19bc248b0a18501472266fc2219e
AntoineVanGelderJnr <AntoineVanGelderJnr@5035072a-db25-11dd-9f21-5bfbb98aebe5>
2011-01-31 06:12:20 +0000

    Completed basic PubSub module Completed auth module Added response helper for villagebus.node requests

--- branches/unstable/villagebus/nodejs/modules/auth.js
+++ branches/unstable/villagebus/nodejs/modules/auth.js
@@ -1,7 +1,5 @@
 
-var redis = require("redis"); // npm install redis
 var fail = require("../common").fail;
-
 console.log("loaded module: auth"); // TODO - __filename
 
 
@@ -14,39 +12,79 @@ exports.session = {
 
 exports.logout = {
   GET : function(request, response, rest) {
+    request.session.data.userid     = -1;
+    request.session.data.username   = "guest";
     request.session.data.authorized = false;
-    request.session.data.username = "guest";
-    // else redirect to login page
-    console.log("redirecting...");
-    response.writeHeader(302, { 
+    response.writeHead(302, { 
       'Location' : '/villagebus.tests.html'
     });
     response.end();
-
-    return undefined;
   }
 };
 
 
+// login -> POST /auth/register { username : <username>, sha1 : <sha1> } -> <userid>
+// ccurl -X POST -d '{"username":"antoine@7degrees.co.za","sha1":"210b263cd3aed1604792c1449058e40d6ae3b86b"}' http://127.0.0.1:8000/auth/login
 exports.login = {
   POST : function(request, response, rest) {
-    // look up username & check hash
     if (!rest.data.username && !rest.data.sha1) {
-      return fail("Invalid login: " + rest.path, "auth", 401);   
+      return response.fin(401, "invalid credentials: " + rest.data);   
     }
-    
-    // check user in DB & return 401 if invalid
-    request.session.data.authorized = true;
-    request.session.data.username = rest.data.username;
-    
-    // else redirect to login page
-    console.log("redirecting...");
-    response.writeHeader(302, { 
-      'Location' : '/villagebus.tests.html'
+    var client = request.session.redis();
+    client.get("username:" + rest.data.username + ":userid", function(error, userid) {
+      if (error) return response.fin(500, error);
+      if (!userid) return response.fin(401, "no such user: " + rest.data.username);
+      client.get("userid:" + userid + ":sha1", function (error, sha1) {
+        if (error) return response.fin(500, error);
+        if (!sha1) return response.fin(401, "no hash for userid: " + userid);
+        if (rest.data.sha1 != sha1) return response.fin(401, "invalid password");
+        request.session.data.userid     = userid;
+        request.session.data.username   = rest.data.username;
+        request.session.data.authorized = true;
+        response.fin(302, request.session.data, { // redirect
+          'Location' : '/villagebus.tests.html'
+        });
+      });
     });
-    response.end();
-    
-    return undefined; 
+  }
+};
+
+
+// register -> POST /auth/register { username : <username>, sha1 : <sha1> } -> <userid>
+// curl -X POST -d '{"username":"antoine@7degrees.co.za","210b263cd3aed1604792c1449058e40d6ae3b86b":"1234"}' http://127.0.0.1:8000/auth/register
+exports.register = {
+  POST : function(request, response, rest) {
+    var client = request.session.redis();    
+    // TODO check if user already exists
+    return client.get("username:" + rest.data.username + ":userid", function(error, userid) {
+      if (error) return response.fin(500, error);
+      if (userid) return response.fin(409, "username already exists");
+      create(rest.data.username, rest.data.sha1);
+    });
+
+    function create(username, sha1) {
+      client.incr("next:userid", function(error, userid) {
+        if (error) return response.fin(500, error);
+        var profile = { userid : userid,
+                        username : username };
+        client.multi()
+            .zadd("users:username", 0, username)
+            .sadd("users:userid", userid)
+            .mset([ "userid:"   + userid   + ":username", username,
+                    "userid:"   + userid   + ":sha1",     sha1,
+                    "userid:"   + userid   + ":profile",  JSON.stringify(profile),
+                    "username:" + username + ":userid",   userid ])
+            .exec(function(error, reply) {
+              if (error) return response.fin(500, error);
+              request.session.data.userid     = userid;
+              request.session.data.username   = username;
+              request.session.data.authorized = true;
+              response.fin(302, userid, { 
+                'Location' : '/villagebus.tests.html'
+              });
+            });
+      });
+    };
   }
 };
 
--- branches/unstable/villagebus/nodejs/villagebus.node.js
+++ branches/unstable/villagebus/nodejs/villagebus.node.js
@@ -1,4 +1,3 @@
-
 // node.js dependencies
 var url  = require("url");
 var http = require("http");
@@ -12,9 +11,11 @@ var fail = require("./common").fail;
 
 // load modules
 var modules = {
-  auth : require("./modules/auth"),
-  test : require("./modules/test"),
-  http : require("./modules/http")
+  auth     : require("./modules/auth"),
+  test     : require("./modules/test"),
+  http     : require("./modules/http"),
+  pubsub   : require("./modules/pubsub"),
+  profiles : require("./modules/profiles"),
 };
 
 // exit handler
@@ -50,6 +51,29 @@ http.ServerResponse.prototype.writeHead = function (status, headers) {
 // main server request handler
 http.createServer(function (request, response) {
 
+  // response helper
+  response.fin = function(status, reply, headers, module) {
+    status  = status  ? status         : 200;
+    headers = headers ? headers        : {};
+    module  = module  ? module + ": "  : "";
+    console.log("fin " + module + status + " " + reply);
+    if (request.session.redis.client) { // close any open redis session
+      try {
+        console.log("quit redis");
+        request.session.redis.client.quit();
+      } catch (e) {
+        console.log("ERROR QUITTING REDIS: " + e);
+      }
+      request.session.redis.client = undefined;
+    }
+    headers["Content-Type"] = "text/plain"; // TODO pass content type e.g. "application/json";
+    response.writeHead(status, headers);
+    if (reply) response.write(JSON.stringify(reply));
+    response.end();
+  };
+
+
+  // read post data
   var data = undefined;
   request.addListener("data", function(chunk) {
     if (!data) data = "";
@@ -65,25 +89,39 @@ http.createServer(function (request, response) {
   });
 
   
+  // handle request
   request.addListener("end", function() {
 
     console.log("\n----------------------------------------------------------------------");
     var a = url.parse(unescape(request.url), true);
+    request.path  = a.pathname.split(/\/+/g).slice(1);
+    request.query = a.query;
+    request.data  = data;
     var rest = {
+      url   : request.url,
       verb  : request.method,
       path  : a.pathname.split(/\/+/g).slice(1),
       query : a.query,
       data  : data
     };
-    console.log("REST : " + JSON.stringify(rest));
+    console.log("REST    : " + JSON.stringify(rest));
 
     // get session
     request.session = sessions.lookupOrCreate(request, {
       lifetime : 3600
     });
+    request.session.redis = function() {
+      if (!request.session.redis.client) {
+        request.session.redis.client = require("redis").createClient();
+        request.session.redis.client.on("error", function(error) { 
+          console.log("REDIS ERROR " + error); 
+        });
+      }
+      return request.session.redis.client;
+    }
     response.setHeader('Set-Cookie', request.session.getSetCookieHeaderValue());
-    console.log(request.session);
-
+    //console.log("Cookie  : " + request.session.getSetCookieHeaderValue());
+    console.log("Session : " + JSON.stringify(request.session));
 
     // dispatch request
     var reply  = { error : "Unknown error for name: '" + name + "'", status : 200 };
@@ -91,52 +129,39 @@ http.createServer(function (request, response) {
     var module = modules[name];
     if (!module) {                            // try to serve as a file
       return paperboy.deliver(config.webroot, request, response)
-              .addHeader("Expires", 300)
-              .addHeader("X-PaperRoute", "Node")
-              .after(function(status) {
-                  //console.log(status + "\t" + request.url + "\t" + request.connection.remoteAddress);
-                });
-
-    } else if (module[rest.verb]) {   // try the REST verbs
-      reply = module[rest.verb](request, response, rest);
-
-    } else if (module["evaluate"]) {  // look for an evaluate function
-      reply = module.evaluate(request, response, rest);
-
+          .addHeader("Expires", 300)
+          .addHeader("X-PaperRoute", "Node")
+          .after(function(status) {
+            //console.log(status + "\t" + request.url + "\t" + request.connection.remoteAddress);
+          });
+      
     } else if (module) {
-      var namefn = rest.path.shift();
-      if (module[namefn]) {
-        if (module[namefn][rest.verb]) {
+      var namefn = rest.path[0];
+      if (module[namefn]) {               // if we have a namefn
+        namefn = rest.path.shift();
+        if (module[namefn][rest.verb]) {  // invoke any REST verb attached to namefn
           reply = module[namefn][rest.verb](request, response, rest);
-        } else {
-          reply = module[namefn](request, response, rest);
+        } else {                          // invoke namefn
+          reply = module[namefn](request, response, rest); 
         }
+      } else if (module[rest.verb]) {     // invoke any REST verbs attached to module
+        reply = module[rest.verb](request, response, rest);
+      } else if (module["evaluate"]) {    // invoke any evaluate function attached to module
+        reply = module.evaluate(request, response, rest);
       } else {
-        reply = fail("Could not resolve name '" + namefn + "' in module: " + name);
-        reply.status = 404;
+        return response.fin(404, "Could not resolve name '" + namefn + "' in module: " + name);
       }
 
     } else {
       reply.status = 420; // enhance your calm
+      return response.fin(420, "enhance your calm");
     }
 
     // reply is undefined if module wants to handle the response itself
     if (reply) {
-      response.writeHead(reply.status ? reply.status : 200, { 
-        //'Content-Type': 'application/json'
-        'Content-Type': 'text/plain' 
-      });
-      response.write(JSON.stringify(reply) + "\n");
-      response.end();
+      return response.fin(200, reply, {}, "villagebus"); // TODO use status from fail above
     } 
-
   });
-
-  
 }).listen(config.port);
 console.log('VillageBus node running at http://127.0.0.1:8000/');
 
-
-function session(request, options) {
-  
-};
